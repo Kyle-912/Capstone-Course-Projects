49163da84170cfc1133a92b0c8a50f6669d8ef30
Kyle-912 <kdt912@outlook.com>
2023-10-26 00:53:53 +0000

    Oct 25, 2023, 8:53 PM

---
+++ rust_lab/first_rust_project/target/debug/deps/first_rust_project-7aee273c6fc23747.d
@@ -0,0 +1,5 @@
+/home/wsl/CEN3907/rust_lab/first_rust_project/target/debug/deps/first_rust_project-7aee273c6fc23747.rmeta: src/main.rs
+
+/home/wsl/CEN3907/rust_lab/first_rust_project/target/debug/deps/first_rust_project-7aee273c6fc23747.d: src/main.rs
+
+src/main.rs:
---
+++ rust_lab/first_rust_project/target/debug/deps/first_rust_project-c3177ad50e712fb1.d
@@ -0,0 +1,5 @@
+/home/wsl/CEN3907/rust_lab/first_rust_project/target/debug/deps/first_rust_project-c3177ad50e712fb1.rmeta: src/main.rs
+
+/home/wsl/CEN3907/rust_lab/first_rust_project/target/debug/deps/first_rust_project-c3177ad50e712fb1.d: src/main.rs
+
+src/main.rs:
