f52a46d9090bdffe9d45941c207262ef588d9408
Kyle-912 <kdt912@outlook.com>
2023-10-30 21:24:28 +0000

    Oct 30, 2023, 5:24 PM

--- elected_lab/src/main.rs
+++ elected_lab/src/main.rs
@@ -153,32 +153,32 @@ fn main() -> ! {
     let mut flash = Flash::new(RGB8::new(255, 255, 255));
     let mut wave = Wave::new(RGB8::new(0, 0, 255));
 
-    let mut mode: u8;
+    let mut mode: u8 = 2;
     let mut nticks: u8 = 5; // Loop delay is ms
     loop {
         x = accel.x as i32;
         y = accel.y as i32;
 
-        if x.abs() > y.abs() {
-            if x > 0 {
-                mode = 0; // +x
-            } else {
-                mode = 2; // -x
-            }
-        } else {
-            if y > 0 {
-                mode = 1; // +y
-            } else {
-                mode = 3; // -y
-            }
-        }
+        // if x.abs() > y.abs() {
+        //     if x > 0 {
+        //         mode = 0; // +x
+        //     } else {
+        //         mode = 2; // -x
+        //     }
+        // } else {
+        //     if y > 0 {
+        //         mode = 1; // +y
+        //     } else {
+        //         mode = 3; // -y
+        //     }
+        // }
 
         if nticks > 4 {
             nticks = 0;
 
             write!(usb, "X: {}, Y: {}\r\n", x, y).unwrap();
             write!(usb, "Updating display...\r\n").unwrap();
-            
+
             pulse.next();
             snake.next();
             flash.next();
